2162e80ee6501ed999524a7f1d7bf01edefa8823
Abrar <AbrarAlsalman@hotmail.com>
2020-11-29 19:08:20 +0300

    Created The Project

---
+++ PicturesOnTheMap/PicturesOnTheMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PicturesOnTheMap
+//
+//  Created by user on 14/04/1442 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
