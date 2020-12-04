c0626a0492c855172fcf83d0a78aced92346e212
Abrar Alsalman <AbrarAlsalman@hotmail.com>
2020-12-05 02:19:56 +0300

    Added Unit Testing for Fetch Image & Photo Functions

--- PicturesOnTheMap/PicruresOnMapTests/PhotoStoreTests.swift
+++ PicturesOnTheMap/PicruresOnMapTests/PhotoStoreTests.swift
@@ -11,30 +11,48 @@ import XCTest
 class PhotoStoreTests: XCTestCase {
     
     let photoStoreObject = PhotoStore()
-
+    
     //MARK: -Test Web Service Request
     
     func testFetchPhotos() {
-
+        
         let completionExpectation = expectation(description: "Execute completion closure.")
         
-       //Test Web Service Request
+        //Test Web Service Request
         photoStoreObject.fetchPhotos {
             (photosResult) -> Void in
-
+            
             XCTAssertEqual(OperationQueue.current,
                            OperationQueue.main,
                            "Completion handler should run on the main thread; it did not.")
             completionExpectation.fulfill()
-            }
+        }
         
-                waitForExpectations(timeout: 1.0, handler: nil)
-   }
-
-    //MARK: -
-    
-    func testProcessPhotosRequest() {
-
+        waitForExpectations(timeout: 1.0, handler: nil)
     }
-
+    
+    //MARK: -Test Image Request
+    
+    func testFetchImage() {
+        
+        let completionExpectation = expectation(description: "Execute completion closure.")
+        
+        //Test Web Service Request
+        photoStoreObject.fetchImage(for: Constants.photoTestData) {
+            (photosResult) -> Void in
+            
+            XCTAssertEqual(OperationQueue.current,
+                           OperationQueue.main,
+                           "Completion handler should run on the main thread; it did not.")
+            completionExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+    
+    //MARK: -
+    func testProcessPhotosRequest() {
+        
+    }
+    
 }
